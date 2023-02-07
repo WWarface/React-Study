62d34044525cc05c95c988ddec8f4c1c49cbec02
Haretone Shulge <2003harik2003@gmail.com>
2023-02-07 16:24:12 +0200

    lesson14

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import s from './Header.module.css'
+
 
 const Header = () => {
     return (
         <header>
-            <img className='header-image' src='https://upload.wikimedia.org/wikipedia/commons/thumb/7/74/C.P._Company_logo.svg/1280px-C.P._Company_logo.svg.png'></img>
+            <img className={s.img} src='https://upload.wikimedia.org/wikipedia/commons/thumb/7/74/C.P._Company_logo.svg/1280px-C.P._Company_logo.svg.png'></img>
         </header>
     );
 }
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,38 +1,25 @@
 import React from 'react';
+import Posts from './Posts';
+import s from './Profile.module.css'
+
 
 const Profile = () => {
     return (
-        <div className='main-content'>
-            <img className='main-content-image' src='https://i.ytimg.com/vi/V33G0n7dQVA/maxresdefault.jpg' />
-            <div className='profile-container'>
-                <div className='profile-picture-container'>
-                    <img className='profile-picture' src='https://dota2.ru/img/heroes/pudge/portrait.jpg'></img>
+        <div className={s.content}>
+            <img className={s.contentImage} src='https://i.ytimg.com/vi/V33G0n7dQVA/maxresdefault.jpg' />
+            <div className={s.profileContainer}>
+                <div>
+                    <img className={s.picture} src='https://dota2.ru/img/heroes/pudge/portrait.jpg'></img>
                 </div>
-                <div className='profile-info-container'>
-                    <h3 className='profile-name'>Pudge</h3>
+                <div>
+                    <h3 className={s.name}>Pudge</h3>
                     <p>Date of birth: 2 january</p>
                     <p>City: Zhmerynka</p>
                     <p>Family Status: not married</p>
                     <p>Family Status: not married</p>
                 </div>
             </div>
-            <div className='posts-container'>
-                <h3 className='posts-text'>My posts</h3>
-                <input typeof='text' className="posts-input" value={'your news...'} onFocus="value=''"></input>
-                <button className='posts-button'>Send</button>
-                <div className='post-sent-container'>
-                    <div className='circle'></div>
-                    <p>Why nobody hook me</p>
-                </div>
-                <div className='post-sent-container'>
-                    <div className='circle'></div>
-                    <p>Why nobody hook me</p>
-                </div>
-                <div className='post-sent-container'>
-                    <div className='circle'></div>
-                    <p>Why nobody hook me</p>
-                </div>
-            </div>
+            <Posts />
         </div>
     );
 }
